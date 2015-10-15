f9fb99027a9721d03ecbb149bb9bf7d8d3e16bdd
José Coelho <jose.alberto.coelho@gmail.com>
2015-10-15 18:04:36 +0100

    always comment your code!

--- app/src/main/java/github/cesarferreira/helloworld/MainActivity.java
+++ app/src/main/java/github/cesarferreira/helloworld/MainActivity.java
@@ -2,7 +2,21 @@ package github.cesarferreira.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+/*
 
+      .-.                                                      
+     (/^\)    I MAY CHOKE MYSELF ON THIS COCK UNTIL I          
+     (\ /)     THROW UP BUT I WON'T SWALLOW THE JIZZ BECAUSE   
+     .-'-.      AT LEAST I HAVE SOME DIGNITY                   
+    /(_I_)\                                                    
+    \\) (//   SPIT ON MY FACE ?!?!?!?!? FINE                   
+     /   \                                                     
+     \ | /      JIZZ UP MY FACE ?!?!?!? A-OKAY                 
+      \|/                                                      
+      /|\         BUT SWALLOW YOUR SPURT ?!??!!? NOW WAY MISTER
+      \|/          I HAVE CLASS                                
+      /Y\     
+*/
 public class MainActivity extends Activity {
 
     @Override
